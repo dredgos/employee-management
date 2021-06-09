f0d478be90aa850a89f0901da326d4470f51379c
Paul Dredge <dredgos@googlemail.com>
2021-06-09 17:12:28 +0100

    captured unsaved files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,23 +6,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use App\Contracts\UserRepositoryInterface\BaseRepositoryInterface;
-use App\Contracts\UserRepositoryInterface\UserRepositoryInterface;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    private $userRepository;
-
-    public function __construct(BaseRepositoryInterface $userRepository)
-    {
-        $this->userRepository = $userRepository;
-    }
-
-    public function getAllUsers()
-    {
-        $user = $userRepository->getAll();
-
-    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contracts\UserRepositoryInterface\UserRepositoryInterface;
 
 class UserController extends Controller
 {
-    //
+    private $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAllUsers()
+    {
+        $user = $userRepository->getAll();
+
+    }
 }
