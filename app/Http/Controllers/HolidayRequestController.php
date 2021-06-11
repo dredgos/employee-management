5836dc8c9a79bbe07e149042a1252d7b0c3aa09f
Sam Archie <samuelarchie1@gmail.com>
2021-06-11 15:32:45 +0100

    creating the basic routes and associated controllers

---
+++ app/Http/Controllers/HolidayRequestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HolidayRequestController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function createHolidayrequest()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HolidayRequestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +19,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(["prefix" => "dashboard"], function () {
+    Route::get("/", [DashboardController::class, "index"])->middleware(['auth'])->name('dashboard');
+    Route::group(["prefix" => "holiday-request"], function () {
+        Route::get("holiday-request", [HolidayRequestController::class, "index"]);
+        Route::post("holiday-request", [HolidayRequestController::class, "createHolidayRequest"]);
+    });
+    Route::group(["prefix" => "training-request"], function () {
+        Route::get("training-request", [TrainingRequestController::class, "index"]);
+         Route::post("training-request", [HolidayRequestController::class, "createTrainingRequest"]);
+    });
+    
+    Route::group(["prefix" => "departments"], function () {
+        Route::get("", [DepartmentController::class, "index"]);
+        
+        Route::group(["prefix" => "roles"], function () {
+            Route::get("", [RoleController::class, "index"]);
+
+
+        });
+    });
+
+    Route::group(["prefix" => "employees"], function () {
+        Route::get("", [EmployeeController::class, "index"]);
+        Route::group(["prefix" => "{id}"], function () {
+            Route::get("", [EmployeeController::class, "show"]);
+        });
+    });
+    
+});
+
+
 
 require __DIR__.'/auth.php';
