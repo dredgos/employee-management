ce3c9b35ddf5f806484c7896e0e9d9545f0f0c24
Sam Archie <samuelarchie1@gmail.com>
2021-06-17 18:27:25 +0100

    minor refactoring

--- resources/views/components/create-user-form.blade.php
+++ resources/views/components/create-user-form.blade.php
@@ -82,13 +82,35 @@
           <input
             name="email"
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-            id="post-code"
+            id="email"
             type="email"
             placeholder="email@example.com"
             @error('email') is-invalid @enderror value="{{old('email')}}"
           />
   
             @error('email')
+              <p class="invalid-feedback mb-2 font-semibold text-red-600">{{$message}}</p>
+            @enderror
+        </div>
+      </div>
+
+      
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="email">
+            Password
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <input
+            name="password"
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+            id="password"
+            type="text"
+            @error('password') is-invalid @enderror value="{{old('password')}}"
+          />
+  
+            @error('password')
               <p class="invalid-feedback mb-2 font-semibold text-red-600">{{$message}}</p>
             @enderror
         </div>
